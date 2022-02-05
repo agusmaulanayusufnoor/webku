e28bac6a4571ad1459379efc98fdb744e65a6f75
agusmaulanayusufnoor <agusmaulanayusufnoor@gmail.com>
2022-02-05 18:18:25 +0700

    perbaikihtaccess

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\StockController;
 use App\Http\Controllers\UploadbaController;
